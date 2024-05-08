458a6d9b7d7382041a5ed81260c898ac33cf6640
hmr53 <h.malshan.53@gmail.com>
2024-05-08 08:28:14 +0530

    TodoService modified to add todo.

--- src/main/java/com/hasith/todoapp/service/TodoService.java
+++ src/main/java/com/hasith/todoapp/service/TodoService.java
@@ -1,5 +1,6 @@
 package com.hasith.todoapp.service;
 
+import com.hasith.todoapp.dao.TodoDao;
 import com.hasith.todoapp.model.Todo;
 
 import java.util.List;
@@ -8,4 +9,5 @@ public interface TodoService {
     public List<Todo> getAllTodos();
     public Todo getSingleTodo(Integer id);
     public void deleteTodo(Integer id);
+    public Todo addTodo(TodoDao todoDao);
 }
