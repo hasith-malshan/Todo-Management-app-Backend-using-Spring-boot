34c93570ed162965462277e192f91dd24175b6c3
hmr53 <h.malshan.53@gmail.com>
2024-05-08 11:47:05 +0530

    H2 database Related Seervices Added.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO TODO(id,description,username,target_date,is_done) VALUES (10001,'Learn AWS','hasith',CURRENT_DATE(),false);
+INSERT INTO TODO(id,description,username,target_date,is_done) VALUES (10002,'Learn React','hasith',CURRENT_DATE(),false);
+INSERT INTO TODO(id,description,username,target_date,is_done) VALUES (10003,'Learn Angular','hasith',CURRENT_DATE(),false);
+INSERT INTO TODO(id,description,username,target_date,is_done) VALUES (10004,'Learn DevOps','hasith',CURRENT_DATE(),false);
